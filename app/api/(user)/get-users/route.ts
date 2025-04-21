8345d6317f3e0864f096155dca05da14b16778c9
John Carl Lecaros <lecarosjc@gmail.com>
2025-04-21 16:55:24 +0800

    Update route.ts

--- app/api/(user)/get-users/route.ts
+++ app/api/(user)/get-users/route.ts
@@ -1,26 +1,23 @@
-import prisma from "@/lib/prisma"
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    try {
-      const users = await prisma.user.findMany({
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          gender: true,
-          school: true,
-          id_no: true,
-          role: true,
-        },
-      })
-      res.status(200).json(users)
-    } catch (error) {
-      console.error("Error fetching users:", error)
-      res.status(500).json({ error: "Failed to fetch users" })
-    }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed!" })
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        gender: true,
+        school: true,
+        id_no: true,
+        role: true,
+      },
+    });
+
+    return NextResponse.json(users, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json({ error: "Failed to fetch users" }, { status: 500 });
   }
 }
